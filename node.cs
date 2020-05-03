9a7886586f7c9e9fd22b2bb60f1ca74d927863be
MinecraftPublisher <44898501+MinecraftPublisher@users.noreply.github.com>
2020-05-03 15:46:49 +0430

    Update node.cs

--- node.cs
+++ node.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Runtime.InteropServices;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
@@ -18,6 +19,19 @@ namespace Swapcoin_Node
         [STAThread]
         static void Main()
         {
+            [DllImport("kernel32.dll")]
+static extern IntPtr GetConsoleWindow();
+
+[DllImport("user32.dll")]
+static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+const int SW_HIDE = 0;
+const int SW_SHOW = 5;
+            var handle = GetConsoleWindow();
+
+// Hide
+ShowWindow(handle, SW_HIDE);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());

